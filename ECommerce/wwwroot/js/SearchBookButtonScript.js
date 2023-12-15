f5d3cc9dcc9213b9d8a2be05e6f37670ce4b2112
yagizerdem <yagizerdem819@gmail.com>
2023-12-15 21:22:34 +0300

    search book funciton added

---
+++ ECommerce/wwwroot/js/SearchBookButtonScript.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    const $list = $(".book-row")
+
+    $("#searchbutton").on("click", () => {
+        const value_from_input = $("#searchinput").val().toLowerCase();
+
+        $list.each(function () {
+            var children = $(this).children();
+            // Check if the search input matches any book title or author
+            if (
+                value_from_input === '' || // Show all books when the search input is empty
+                value_from_input.includes(children.eq(2).text().toLowerCase()) ||
+                value_from_input.includes(children.eq(3).text().toLowerCase())
+            ) {
+                $(this).fadeIn(); // Show the book
+            } else {
+                $(this).fadeOut(); // Hide the book
+            }
+        });
+    });
+});
